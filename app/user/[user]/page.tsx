8186e9479aa4eeb8c813b38c6289a5d1348ce703
imdebamrita <imdebamrita@gmail.com>
2024-06-29 05:05:34 +0530

    feed content update and navbar ui fixing

--- app/user/[user]/page.tsx
+++ app/user/[user]/page.tsx
@@ -20,24 +20,32 @@ const FEED_ITEMS = [
     username: "shahank",
     descripton: "i planted a tree and i feel like i saved the world",
     picUrl: "/tree.jpg",
+    avatarUrl: "/avatar.jpg",
+    date: "09/11/2001",
   },
   {
     id: 1,
     username: "shahank2",
     descripton: "i planted a tree and i feel like i saved the world",
     picUrl: "/tree.jpg",
+    avatarUrl: "/avatar.jpg",
+    date: "09/11/2001",
   },
   {
     id: 2,
     username: "shahank2",
     descripton: "i planted a tree and i feel like i saved the world",
     picUrl: "/tree.jpg",
+    avatarUrl: "/avatar.jpg",
+    date: "09/11/2001",
   },
   {
     id: 3,
     username: "shahank2",
     descripton: "i planted a tree and i feel like i saved the world",
     picUrl: "/tree.jpg",
+    avatarUrl: "/avatar.jpg",
+    date: "09/11/2001",
   },
 ];
 
