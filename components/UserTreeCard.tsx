b379c6b9aaeebf63c8435cf741c3b48f145f6e19
Phynxxx <mail2syamantak@gmail.com>
2024-06-30 02:09:31 +0530

    minor changes

--- components/UserTreeCard.tsx
+++ components/UserTreeCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { UserTreeItem } from "@/lib/types";
 import { pb } from "@/lib/pbClient";
