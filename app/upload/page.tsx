6437828de28fdc4326d9814514c88d76e5e32dc4
shahank42 <shounakghosh42@gmail.com>
2024-06-29 21:01:17 -0400

    cosmetic

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -26,6 +26,7 @@ import { nftreeContract } from "@/lib/web3";
 import { client } from "@/lib/thirdWebClient";
 import { Wallet, createWallet, inAppWallet } from "thirdweb/wallets";
 import { sendAndConfirmTransaction } from "thirdweb";
+import { useRouter } from "next/navigation";
 
 // Utility function to convert data URL to Blob
 const dataURLToBlob = (dataURL: string): Blob => {
@@ -70,6 +71,9 @@ function Page() {
   const [selectedFile, setSelectedFile] = useState<Blob>();
   const [latitude, setLatitude] = useState<string>("");
   const [longitude, setLongitude] = useState<string>("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const router = useRouter();
 
   console.log(`lat:${latitude}, long: ${longitude}`);
 
@@ -126,6 +130,7 @@ function Page() {
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     if (selectedFile) {
+      setSubmitted(true);
       // console.log(Array.from(formData));
       if (!activeAccount) throw "Account not active";
 
@@ -163,7 +168,7 @@ function Page() {
         .collection("tree_images")
         .create(newFormData);
 
-      // TODO: redirect to individual tree page
+      router.push(`/trees/${treeUUID}`);
     }
   };
 
@@ -291,7 +296,77 @@ function Page() {
               />
             </div>
 
-            <Button type="submit">Submit new tree</Button>
+            {submitted ? (
+              <>
+                <Button disabled>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 24 24"
+                  >
+                    <defs>
+                      <filter id="svgSpinnersGooeyBalls10">
+                        <feGaussianBlur
+                          in="SourceGraphic"
+                          result="y"
+                          stdDeviation="1.5"
+                        ></feGaussianBlur>
+                        <feColorMatrix
+                          in="y"
+                          result="z"
+                          values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -7"
+                        ></feColorMatrix>
+                        <feBlend in="SourceGraphic" in2="z"></feBlend>
+                      </filter>
+                    </defs>
+                    <g
+                      fill="currentColor"
+                      filter="url(#svgSpinnersGooeyBalls10)"
+                    >
+                      <circle cx="4" cy="12" r="3">
+                        <animate
+                          attributeName="cx"
+                          calcMode="spline"
+                          dur="0.75s"
+                          keySplines=".56,.52,.17,.98;.56,.52,.17,.98"
+                          repeatCount="indefinite"
+                          values="4;9;4"
+                        ></animate>
+                        <animate
+                          attributeName="r"
+                          calcMode="spline"
+                          dur="0.75s"
+                          keySplines=".56,.52,.17,.98;.56,.52,.17,.98"
+                          repeatCount="indefinite"
+                          values="3;8;3"
+                        ></animate>
+                      </circle>
+                      <circle cx="15" cy="12" r="8">
+                        <animate
+                          attributeName="cx"
+                          calcMode="spline"
+                          dur="0.75s"
+                          keySplines=".56,.52,.17,.98;.56,.52,.17,.98"
+                          repeatCount="indefinite"
+                          values="15;20;15"
+                        ></animate>
+                        <animate
+                          attributeName="r"
+                          calcMode="spline"
+                          dur="0.75s"
+                          keySplines=".56,.52,.17,.98;.56,.52,.17,.98"
+                          repeatCount="indefinite"
+                          values="8;3;8"
+                        ></animate>
+                      </circle>
+                    </g>
+                  </svg>
+                </Button>
+              </>
+            ) : (
+              <Button type="submit">Submit new tree</Button>
+            )}
             {/* <TransactionButton
               transaction={() =>
                 safeMint({
--- components/UpdateForm.tsx
+++ components/UpdateForm.tsx
@@ -34,6 +34,7 @@ import {
   verifyDaily,
 } from "@/thirdweb/43113/0xdcee2dd10dd46086cc1d2b0825a11ffc990e6eff";
 import { nftreeContract } from "@/lib/web3";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   nameId: z.string().nonempty("Please select a tree"),
@@ -66,6 +67,9 @@ function UpdateForm({ userTrees }: { userTrees: RecordModel[] }) {
   const webcamRef = useRef<Webcam>(null);
   const [url, setUrl] = useState("/tree.jpg");
   const [selectedFile, setSelectedFile] = useState<Blob | null>(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -104,6 +108,7 @@ function UpdateForm({ userTrees }: { userTrees: RecordModel[] }) {
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     if (selectedFile) {
+      setSubmitted(true);
       if (!activeAccount) throw "Account not active";
 
       const tokenId = (await pb.collection("trees").getOne(data.nameId))
@@ -117,7 +122,7 @@ function UpdateForm({ userTrees }: { userTrees: RecordModel[] }) {
         account: activeAccount,
       });
 
-      console.log(reciept)
+      console.log(reciept);
 
       const formData = new FormData();
       formData.append("picUrl", selectedFile);
@@ -130,6 +135,8 @@ function UpdateForm({ userTrees }: { userTrees: RecordModel[] }) {
       } catch (error) {
         console.error("Failed to add image:", error);
       }
+
+      router.push("/")
     }
   };
 
@@ -208,7 +215,77 @@ function UpdateForm({ userTrees }: { userTrees: RecordModel[] }) {
               />
             </div>
 
-            <Button type="submit">Add image for tree</Button>
+            {submitted ? (
+              <>
+                <Button disabled>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 24 24"
+                  >
+                    <defs>
+                      <filter id="svgSpinnersGooeyBalls10">
+                        <feGaussianBlur
+                          in="SourceGraphic"
+                          result="y"
+                          stdDeviation="1.5"
+                        ></feGaussianBlur>
+                        <feColorMatrix
+                          in="y"
+                          result="z"
+                          values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -7"
+                        ></feColorMatrix>
+                        <feBlend in="SourceGraphic" in2="z"></feBlend>
+                      </filter>
+                    </defs>
+                    <g
+                      fill="currentColor"
+                      filter="url(#svgSpinnersGooeyBalls10)"
+                    >
+                      <circle cx="4" cy="12" r="3">
+                        <animate
+                          attributeName="cx"
+                          calcMode="spline"
+                          dur="0.75s"
+                          keySplines=".56,.52,.17,.98;.56,.52,.17,.98"
+                          repeatCount="indefinite"
+                          values="4;9;4"
+                        ></animate>
+                        <animate
+                          attributeName="r"
+                          calcMode="spline"
+                          dur="0.75s"
+                          keySplines=".56,.52,.17,.98;.56,.52,.17,.98"
+                          repeatCount="indefinite"
+                          values="3;8;3"
+                        ></animate>
+                      </circle>
+                      <circle cx="15" cy="12" r="8">
+                        <animate
+                          attributeName="cx"
+                          calcMode="spline"
+                          dur="0.75s"
+                          keySplines=".56,.52,.17,.98;.56,.52,.17,.98"
+                          repeatCount="indefinite"
+                          values="15;20;15"
+                        ></animate>
+                        <animate
+                          attributeName="r"
+                          calcMode="spline"
+                          dur="0.75s"
+                          keySplines=".56,.52,.17,.98;.56,.52,.17,.98"
+                          repeatCount="indefinite"
+                          values="8;3;8"
+                        ></animate>
+                      </circle>
+                    </g>
+                  </svg>
+                </Button>
+              </>
+            ) : (
+              <Button type="submit">Add image for tree</Button>
+            )}
           </form>
         </Form>
       </div>
